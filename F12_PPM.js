1e924450c5aa16fa0d65f5259cd179f558fd8277
Jakub Lesinski <146775007+JakubLesinski30@users.noreply.github.com>
2023-11-10 19:24:50 +0100

    Źródło F12_PPM.js

---
+++ F12_PPM.js
@@ -0,0 +1,9 @@
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+}, false);
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'F12') {
+    e.preventDefault();
+  }
+}, false);
